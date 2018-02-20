ffd0e14cf9a3ebf7ee66c44367d1a3b30eeaa53a
Miguel Santiago Puerto <ms.puerto@uniandes.edu.co>
2018-02-19 22:40:12 -0500

    Implemented post funcionality test

--- ghostTestCypress/cypress/integration/ghost_post_setup_test.js
+++ ghostTestCypress/cypress/integration/ghost_post_setup_test.js
@@ -1,7 +1,7 @@
 /* This test is intended to run after ghost_post_install_test.js */
 
 describe('Ghost Tests - 2', function () {
-
+/*
     //Check if environment it's ok
     it('.should() - confirm ghost is online', function () {
       cy.visit('http://localhost:2368')
@@ -24,12 +24,11 @@ describe('Ghost Tests - 2', function () {
         cy.get('#ember448').type('my@email.com')
         cy.get('#ember450').type('pwd12345678')
         cy.get('#ember460 > span').click()
-        cy.title().should('include', 'Content')
-        cy.get('#ember861 > svg').click()
-        cy.get('#ember1100').click()
+        cy.get('.gh-nav-menu').click()
+        cy.get('.user-menu-signout').click()
         cy.title().should('include', 'Sign In')
     })
-
+*/
     
     //Post an entry in a blog
     it('.should() - logout admin account', function(){
@@ -38,12 +37,10 @@ describe('Ghost Tests - 2', function () {
         cy.get('#ember450').type('pwd12345678')
         cy.get('#ember460 > span').click()
         cy.title().should('include', 'Content')
-        cy.get('#ember876').click()
-        cy.get('#ember1118').type('Test post Title')
-        cy.get('.CodeMirror-line').type('Content for test post')
+        cy.get('.gh-nav-main-editor').contains('New story').click()
+        cy.get('.gh-editor-title').type('Test post Title')
     })
-    
-
 
 })
  
+
